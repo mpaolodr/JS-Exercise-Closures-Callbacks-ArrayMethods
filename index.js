caaa1e72bd984bc5151e3fd6ad989530a4a80fa0
mpaolodr <mpaolodr_1504@yahoo.com>
2019-11-12 17:20:34 -0500

    finished firstNamesAllCaps problem

--- index.js
+++ index.js
@@ -222,8 +222,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
  */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const allCaps = runners.map(function(name) {
+    return name.first_name.toUpperCase();
+  });
+
+  return allCaps;
 }
 
 /**
